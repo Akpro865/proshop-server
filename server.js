bef6eb5099160817dc34095a57372dc7deb959ec
Akpro865 <infinityak865@gmail.com>
2023-02-09 22:32:03 +0530

    cors changed again 9

--- server.js
+++ server.js
@@ -32,9 +32,10 @@ app.use(cors({
 	credentials: true,
 	headers: {
         "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin" : "*",
-    	"Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-    	'Access-Control-Allow-Headers': 'Content-Type, Accept, Origin, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+    	'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Content-Type, Authorization',
+    	'Access-Control-Allow-Methods': '*',
+    	"Content-Type": "application/json"
 
     },
 }))
