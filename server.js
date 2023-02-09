aad500b866ff6fa3f4aeade4d3c62fdead45159b
Akpro865 <infinityak865@gmail.com>
2023-02-09 22:20:08 +0530

    cors changed again 7

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
         "Access-Control-Allow-Credentials": true,
         "Access-Control-Allow-Origin" : "*",
     	"Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
-    	'Access-Control-Allow-Headers', 'Content-Type,Accept',
+    	'Access-Control-Allow-Headers': 'Content-Type,Accept',
     	'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
 
     },
