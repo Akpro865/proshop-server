9fdedbe75ccc7ace9468a7f2266090e7d39adb28
Akpro865 <infinityak865@gmail.com>
2023-02-11 07:36:11 +0530

    9

--- controller/auth.js
+++ controller/auth.js
@@ -5,10 +5,11 @@ const asyncHandler = require('express-async-handler')
 
 // user register
 const register = asyncHandler(async(req, res)=>{
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Content-Type", "application/json");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 	const { name, email, password } = req.body
 
 	if(!email || !name || !password){
